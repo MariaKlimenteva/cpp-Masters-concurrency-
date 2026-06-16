1e6af5f8c986b7639f2ce2ab7e3d769fed4f91af
Klimentieva <klimentieva.maria@h-partners.com>
2026-06-16 15:45:10 +0300

    - create_task refactoring so that it can be used with lambdas/functors - hide global mutex, push/pop into class - synced logs using osyncstream
    TBD: jthreads should be used to avoid join and detach in user's code

--- coe_queue.cc
+++ coe_queue.cc
@@ -1,92 +1,88 @@
-#include <fstream>
 #include <functional>
 #include <future>
 #include <iostream>
 #include <mutex>
 #include <queue>
-#include <sstream>
 #include <thread>
 #include <tuple>
 #include <type_traits>
 #include <utility>
 #include <vector>
+#include <syncstream>
 
 using task_t = std::move_only_function<int()>;
 
-template <typename F, typename... Args> auto create_task(F f, Args &&... args) {
-  std::packaged_task<std::remove_pointer_t<F>> tsk{f};
-  auto fut = tsk.get_future();
-  task_t t{[ct = std::move(tsk),
-            args = std::make_tuple(std::forward<Args>(args)...)]() mutable {
-    std::apply([ct = std::move(ct)](auto &&... args) mutable { ct(args...); },
-               std::move(args));
+template <typename F, typename... Args> 
+auto create_task(F&& f, Args &&... args) {
+  using result_type = std::invoke_result_t<std::decay_t<F>, std::decay_t<Args>...>;
+  auto bound_task = [func = std::forward<F>(f), 
+                    tup = std::make_tuple(std::forward<Args>(args)...)]() mutable {
+    return std::apply(std::move(func), std::move(tup));
+  };
+  std::packaged_task<result_type()> p_task{std::move(bound_task)};
+  auto fut = p_task.get_future();
+  task_t t{[p_task = std::move(p_task)]() mutable {
+    p_task();
     return 0;
   }};
 
   return std::make_pair(std::move(t), std::move(fut));
 }
 
-std::queue<task_t> task_queue;
-std::mutex task_queue_mutex;
-std::mutex cout_mutex;
+class ThreadSafeQueue {
+private:
+  std::queue<task_t> queue_;
+  std::mutex mutex_;
+  std::condition_variable cv_;
 
-void safe_push(task_t &&t) {
-  std::lock_guard<std::mutex> lk{task_queue_mutex};
-  task_queue.push(std::move(t));
-}
-
-bool safe_pop(task_t &out) {
-  std::lock_guard<std::mutex> lk{task_queue_mutex};
-  if (task_queue.empty()) {
-    return false;
+public:
+  void push(task_t &&t) {
+    {
+      std::lock_guard<std::mutex> lk{mutex_};
+      queue_.push(std::move(t));
+    }
+    cv_.notify_one();
   }
-  out = std::move(task_queue.front());
-  task_queue.pop();
-  return true;
-}
 
-void consumer_thread_func() {
+  task_t pop() {
+    std::unique_lock lk{mutex_};
+    cv_.wait(lk, [this] { return !queue_.empty(); });
+    task_t res = std::move(queue_.front());
+    queue_.pop();
+    return res;
+  }
+};
+
+void consumer_thread_func(ThreadSafeQueue &queue, std::mutex &cout_mutex) {
   try {
     for (;;) {
-      task_t cur;
-      if (!safe_pop(cur)) {
-        std::this_thread::yield();
-        continue;
-      }
+      task_t cur = queue.pop();
       int res = std::move(cur)();
       if (res == -1) {
-        task_t sentinel{[] { return -1; }};
-        safe_push(std::move(sentinel));
+        queue.push([] { return -1; });
         break;
       }
     }
-  } catch (std::exception &e) {
-    std::cout << "\n" << e.what() << "\n";
-    std::terminate();
-  } catch (...) {
-    std::cout << "\nSomething wrong in thread function\n";
-    std::terminate();
+  } catch (const std::exception &e) {
+      std::lock_guard lk{cout_mutex};
+      std::cerr << "\nException in thread: " << e.what() << "\n";
   }
 }
 
 int fn1(int x, int y, int z) {
-  std::lock_guard<std::mutex> lk{cout_mutex};
-  std::cout << "a";
+  std::osyncstream{std::cout} << "a";
   return x + y + z;
 }
 
 double fn2(std::vector<int> v) {
-  std::lock_guard<std::mutex> lk{cout_mutex};
-  std::cout << "b";
+  std::osyncstream{std::cout} << "a";
   return v.size() + 0.5;
 }
 
 void fn3() {
-  std::lock_guard<std::mutex> lk{cout_mutex};
-  std::cout << "c";
+  std::osyncstream{std::cout} << "c"; 
 }
-
-void test_queue(int ntasks) {
+void test_queue(ThreadSafeQueue& queue, int ntasks) {
   std::future<int> first_future;
   std::future<double> second_future;
 
@@ -94,14 +90,14 @@ void test_queue(int ntasks) {
     switch (jdx % 3) {
     case 0: {
       auto &&[t, f] = create_task(fn1, 1, 2, 3);
-      safe_push(std::move(t));
+      queue.push(std::move(t));
       first_future = std::move(f);
       break;
     }
     case 1: {
       std::vector v{1, 2, 3};
       auto &&[t, f] = create_task(fn2, v);
-      safe_push(std::move(t));
+      queue.push(std::move(t));
       second_future = std::move(f);
       break;
     }
@@ -111,34 +107,28 @@ void test_queue(int ntasks) {
       first_future.get();
       second_future.get();
 #endif
-      safe_push(std::move(t));
+      queue.push(std::move(t));
       break;
     }
     }
   }
 
   // put final task
-  task_t sentinel{[] { return -1; }};
-  safe_push(std::move(sentinel));
-}
-
-void myhandler() {
-  std::cout << "\nterminate happened\n";
-  abort();
+  queue.push([] { return -1; });
 }
 
 int main() {
   int nthreads = 3;
   int ntasks = 200;
-
-  std::set_terminate(myhandler);
+  ThreadSafeQueue queue;
+  std::mutex cout_mutex;
 
   std::vector<std::thread> consumers;
   for (int i = 0; i < nthreads; ++i)
-    consumers.emplace_back(consumer_thread_func);
+    consumers.emplace_back(consumer_thread_func, std::ref(queue), std::ref(cout_mutex));
 
   try {
-    test_queue(ntasks);
+    test_queue(queue, ntasks);
 
     for (int i = 0; i < nthreads; ++i)
       consumers[i].join();
@@ -147,10 +137,5 @@ int main() {
     return 0;
   } catch (std::exception &e) {
     std::cout << "\n" << e.what() << "\n";
-  } catch (...) {
-    std::cout << "\nSomething wrong\n";
-  }
-
-  for (int i = 0; i < nthreads; ++i)
-    consumers[i].detach();
+  } 
 }
