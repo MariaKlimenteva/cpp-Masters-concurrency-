db395cf61fc2dff2d72bce4fc593cda43c18d164
Klimentieva <klimentieva.maria@h-partners.com>
2026-06-16 14:33:35 +0300

    rewrote the code in std::move_only_function it permits to get an explicit segfault because an API race occurs here (compiled with clang++ -std=c++2b coe_queue.cc -o coe_queue -pthread)

--- coe_queue.cc
+++ coe_queue.cc
@@ -11,49 +11,7 @@
 #include <utility>
 #include <vector>
 
-// std::move_only_function replacement while it is not ready
-
-template <typename T> class fire_once;
-
-template <typename R, typename... Args> class fire_once<R(Args...)> {
-  std::unique_ptr<void, void (*)(void *)> ptr{nullptr, +[](void *) {}};
-  R (*invoke)(void *, Args...) = nullptr;
-
-public:
-  fire_once() = default;
-  fire_once(fire_once &&) = default;
-  fire_once &operator=(fire_once &&) = default;
-
-  template <typename F> fire_once(F &&f) {
-    auto pf = std::make_unique<F>(std::move(f));
-    invoke = +[](void *pf, Args &&... args) -> R {
-      F *f = reinterpret_cast<F *>(pf);
-      return (*f)(std::forward<Args>(args)...);
-    };
-    ptr = {pf.release(), [](void *pf) {
-            F *f = reinterpret_cast<F *>(pf);
-            delete f;
-          }};
-  }
-
-  R operator()(Args &&... args) && {
-    R ret = invoke(ptr.get(), std::forward<Args>(args)...);
-    clear();
-    return std::move(ret);
-  }
-
-  void clear() {
-    invoke = nullptr;
-    ptr.reset();
-  }
-
-  explicit operator bool() const { return static_cast<bool>(ptr); }
-};
-
-// generic (type-erased) task to put on queue
-// returns -1 if it is special signalling task (end of work for consumers)
-// otherwise do what it shall and return 0
-using task_t = fire_once<int()>;
+using task_t = std::move_only_function<int()>;
 
 template <typename F, typename... Args> auto create_task(F f, Args &&... args) {
   std::packaged_task<std::remove_pointer_t<F>> tsk{f};
